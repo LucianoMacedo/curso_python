6a12244ffc9bb0513d2ed9c087c5c785c2258a96
Luciano Macedo <macedoluck2020@gmail.com>
2025-05-08 12:43:18 -0300

    aula 8 exercicio de variaveis

---
+++ aula8.py
@@ -0,0 +1,10 @@
+nome = "Luciano"
+sobrenome = "Macedo"
+idade = 28
+ano_de_nascimento = 1996
+maior_de_idade = idade >= 18
+altura_em_metros = 1.60
+
+print("Meu nome é :",nome,"\n","Meu Sobrenome é :",sobrenome,
+      "\n","Minha idade é :",idade,"\n","Meu ano de nascimento :",ano_de_nascimento,
+      "\n","Eu sou maior de idade :",maior_de_idade,"\n","Minha altura é :",altura_em_metros)
